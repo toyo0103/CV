74af3b98b76d537a216021503f1c1dc2d1d1a7a0
steventsai <steventsai@urtoday.com>
2018-04-10 13:18:38 +0800

    測試右鍵抓框框資料

--- background.js
+++ background.js
@@ -1,29 +1,25 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
-  });
+  // chrome.storage.sync.set({color: '#3aa757'}, function() {
+  //   console.log("The color is green.");
+  // });
 
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'www.instagram.com'},
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+  // chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  //   chrome.declarativeContent.onPageChanged.addRules([{
+  //     conditions: [new chrome.declarativeContent.PageStateMatcher({
+  //       pageUrl: {hostEquals: 'www.instagram.com'},
+  //     })
+  //     ],
+  //         actions: [new chrome.declarativeContent.ShowPageAction()]
+  //   }]);
+  // });
 });
 
-chrome.contextMenus.create({
-  title: "first",
-  contexts: ["selection"],
-  onclick: function() {
-    alert('first');
-  }
-});
+function CallCrawlerPost(info)
+{
+  chrome.tabs.executeScript({code:'CrawThisPost()'});
+  //chrome.tabs.create({url: "http://maps.google.com/maps?q=" + searchstring});
+}
+
+chrome.contextMenus.create({title: "CV訂單統計", contexts:["page"], onclick: CallCrawlerPost});
--- js/myscript.js
+++ js/myscript.js
@@ -1,19 +1,43 @@
 $(function(){
-    let rows = $('div._6d3hm._mnav9');
-    $.each(rows,function(key,row){
-        let boxs = $(row).find('._e3il2');
+    // let rows = $('div._6d3hm._mnav9');
+    // $.each(rows,function(key,row){
+    //     let boxs = $(row).find('._e3il2');
         
-        $(boxs).click(function(){
-            var img = $(this).find('img')[0];
-            console.log($(img).prop('alt'));
-        });
+    //     $(boxs).click(function(){
+    //         var img = $(this).find('img')[0];
+    //         console.log($(img).prop('alt'));
+    //     });
 
-        $(boxs).each(function(){
-            //$(this).click();
-        });
+    //     $(boxs).each(function(){
+    //         //$(this).click();
+    //     });
 
-        $(boxs).promise().done(function(){
-            console.log('123');
-        });
-    });
+    //     $(boxs).promise().done(function(){
+    //         console.log('123');
+    //     });
+    // });
 })
+
+document.addEventListener("mousedown",function(event){
+    //right click
+    if(event.button == 2){
+        console.log($(this));
+        console.log($(this).parents('pre.default.prettyprint.prettyprinted'));
+        if($(this).parent('.default.prettyprint.prettyprinted').length !==0){
+            console.log('has');
+        }
+    }
+});
+
+// document.addEventListener("mousedown", function(event){
+//     console.log(event.fromElement);
+//     //right click
+//     if(event.button == 2) { 
+//         //clickedEl = event.target;
+//     }
+// }, true);
+
+function CrawThisPost()
+{
+    console.log('CrawThisPost');
+}
