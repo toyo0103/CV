d6b078a5b96c6078faeebf64ad25356202a50d70
steventsai <steventsai@urtoday.com>
2018-04-10 14:15:46 +0800

    更換Logo

--- js/myscript.js
+++ js/myscript.js
@@ -1,43 +1,22 @@
-$(function(){
-    // let rows = $('div._6d3hm._mnav9');
-    // $.each(rows,function(key,row){
-    //     let boxs = $(row).find('._e3il2');
-        
-    //     $(boxs).click(function(){
-    //         var img = $(this).find('img')[0];
-    //         console.log($(img).prop('alt'));
-    //     });
+var rightClickFocusPost = null;
+const articleLookLike = 'div[role="article"]';
 
-    //     $(boxs).each(function(){
-    //         //$(this).click();
-    //     });
-
-    //     $(boxs).promise().done(function(){
-    //         console.log('123');
-    //     });
-    // });
-})
-
-document.addEventListener("mousedown",function(event){
-    //right click
-    if(event.button == 2){
-        console.log($(this));
-        console.log($(this).parents('pre.default.prettyprint.prettyprinted'));
-        if($(this).parent('.default.prettyprint.prettyprinted').length !==0){
-            console.log('has');
-        }
+$(document).on('mousedown',articleLookLike,function(e){
+    if(e.button === 2){
+        //Right Click
+        rightClickFocusPost = $(this);
     }
 });
 
-// document.addEventListener("mousedown", function(event){
-//     console.log(event.fromElement);
-//     //right click
-//     if(event.button == 2) { 
-//         //clickedEl = event.target;
-//     }
-// }, true);
+$(function(){
+
+})
 
 function CrawThisPost()
 {
-    console.log('CrawThisPost');
+    if(rightClickFocusPost !== null){
+        console.log(rightClickFocusPost);
+    }else{
+        alert('非Facebook貼文!!')
+    }
 }
